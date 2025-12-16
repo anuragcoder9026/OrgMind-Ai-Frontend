8e9f01aa99064de2790cf368edc8793afc937f30
Anurag Singh <iitpatelanurag2003@gmail.com>
2025-12-16 12:31:33 +0530

    rag chatbot

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -3,8 +3,7 @@ import 'orgmindai-chatbot-widget/style.css';
 export default function Chatbot() {
     return (
         <ChatBotWidget
-            apiKey="sk_org_7b4ff4cf6236f6bd5e534fa1286ff1f778b72b5c79385459"
-            apiBaseUrl="https://orgmind-ai-backend.onrender.com"
+            apiKey="sk_org_e63bd2690a337ce59c66e6161e392d7c189d6e7ed9e8f047"
         />
     );
 }
